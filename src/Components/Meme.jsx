4ba6d37e9a435a12ea0d70be9c55c19fa692bdca
unknown <briaokm@gmail.com>
2024-02-23 22:24:20 +0300

    Adding in the first Files

---
+++ src/Components/Meme.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+export default function Meme(){
+    return(
+        <main>
+            <form className="form">
+                <div>
+                    <label className="form--label">Top text</label>
+                    <input
+                        type="text"
+                        placeholder="Shut up"
+                        className="form--input"
+                    />
+                </div>
+                <div>
+                    <label className="form--label">Bottom text</label>
+                    <input
+                        type="text"
+                        placeholder="and take my money"
+                        className="form--input"
+                    />
+                </div>
+                <button
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+        
+        
+    )
+}
