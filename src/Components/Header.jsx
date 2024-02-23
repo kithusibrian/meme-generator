b579070ad993efd78dd9ee1a5642413c3330a8a8
unknown <briaokm@gmail.com>
2024-02-23 22:55:01 +0300

    Adding in Files

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="header">
             <img className="header--img" src={meme} alt="picha ya meme generator"/>
-            <h2 className="header--title">Hillarious</h2>
+            <h2 className="header--title">Hillarious In Your Diabz</h2>
             <h4 className="header--project">React Project By @BrianKithusi</h4>
         </header>
     )
